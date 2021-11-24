db5a980f602556ea592bc456be3f11698e5fb599
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-24 21:43:04 +0530

    sample

--- output.tf
+++ output.tf
@@ -15,15 +15,3 @@ output "abc1" {
   value = var.abc1
 }
 
-# String Data type
-variable "sample1" {
-  default = "Hello World"
-}
-# Number data type
-variable "sample2" {
-  default = 100
-}
-# Boolean Data type
-variable "sample3" {
-  default = true
-}
