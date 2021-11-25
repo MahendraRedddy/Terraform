d260817e4f3af87e2a752385f1a7f150333186b8
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-25 18:15:17 +0530

    data

--- output.tf
+++ output.tf
@@ -32,7 +32,7 @@ variable "samplex" {
 }
 
 output "samplex" {
-  value = "value 1=${var.samplex[0]},value 3=${var.samplex[2]}"
+  value = "value =${var.samplex[0]},value 3=${var.samplex[2]}"
 }
 
 
