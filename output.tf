a07a63ebcc174ead04f187da53d1c74e7d79cc19
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-25 15:42:33 +0530

    abc2

--- output.tf
+++ output.tf
@@ -9,9 +9,16 @@ output "abc" {
   value =var.abc
 }
 
-variable "abc1" {}
+variable "abc1" {
+}
 
 output "abc1" {
   value = var.abc1
 }
 
+ variable "abc2" {
+   default = 200
+ }
+output "abc2" {
+  value =var.abc2
+}
