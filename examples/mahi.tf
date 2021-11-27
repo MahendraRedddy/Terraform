0289140172307a24a624b7938dbf3d8f13b81422
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-27 18:09:18 +0530

    vars

--- examples/mahi.tf
+++ examples/mahi.tf
@@ -2,23 +2,21 @@ provider "aws" {
   region= "us-east-1"
 }
 
-resource "aws_instance" "mahi" {
-  ami = "ami-0855cab4944392d0a"
+resource  "aws_instance"  "mahe" {
+  ami  = "ami-0855cab4944392d0a"
 
   instance_type = "t2.micro"
   tags = {
-    Name = "Mahi"
+    Name = "Mahe"
   }
 }
 output "public_ip" {
-  value = "aws_instance.mahi.public_ip"
-
-
+  value = "aws_instance.mahe.public_ip"
 }
 
 resource "aws_ec2_tag" "tags" {
   count = "1"
-  resource_id = "aws_instance.mahi.*.private_ip"
+  resource_id = "aws_instance.mahe.*.private_ip"
   key         = "Name"
   value       = "1"
 }
