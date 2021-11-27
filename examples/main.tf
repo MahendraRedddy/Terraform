36dfbbeb780d346725b7126777944b01178bb3c2
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-27 20:56:01 +0530

    vars

--- examples/main.tf
+++ examples/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_security_group" "allow-sample" {
   name        = "Allow Sample"
   description = "Allow sample traffic"
-
+  vpc_id      = "vpc-0359a51174dc9ae11"
   ingress = [
     {
       description      = "TLS from VPC"
