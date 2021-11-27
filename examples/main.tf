80fe922390fc465af453816df100a2e0cfe308af
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-27 20:11:04 +0530

    var

--- examples/main.tf
+++ examples/main.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "sample" {
   ami           = "ami-0855cab4944392d0a"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_sample.id,"sg-05e68a880b653697b"]
+
   tags = {
     Name = "sample"
   }
@@ -16,7 +17,8 @@ resource "aws_security_group" "allow_sample" {
   name        = "allow_sample"
   description = "Allow sample traffic"
 
-  ingress = {
+  ingress = [
+    {
     description      = "TLS from VPC"
     from_port        = 22
     to_port          = 22
@@ -24,8 +26,8 @@ resource "aws_security_group" "allow_sample" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = []
   }
-
-  egress =
+]
+  egress =[
    {
     from_port        = 0
     to_port          = 0
@@ -33,7 +35,7 @@ resource "aws_security_group" "allow_sample" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
+]
   tags = {
     Name = "allow_sample"
   }
