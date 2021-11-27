f938b40709eece0d8003b2f7d74374e7413f0781
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-27 20:04:39 +0530

    vars

--- examples/main.tf
+++ examples/main.tf
@@ -16,8 +16,7 @@ resource "aws_security_group" "allow_sample" {
   name        = "allow_sample"
   description = "Allow sample traffic"
 
-  ingress =[
-    {
+  ingress = {
     description      = "TLS from VPC"
     from_port        = 22
     to_port          = 22
@@ -25,7 +24,7 @@ resource "aws_security_group" "allow_sample" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = []
   }
-]
+
   egress =
    {
     from_port        = 0
