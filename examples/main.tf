c8cbcbe01df71f0598383f58980db720893e1007
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-27 20:42:51 +0530

    var

--- examples/main.tf
+++ examples/main.tf
@@ -2,19 +2,8 @@ provider "aws" {
   region ="us-east-1"
 }
 
-resource "aws_instance" "sample" {
-
-  ami           = "ami-0855cab4944392d0a"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_sample.id,"sg-05e68a880b653697b"]
-
-  tags = {
-    Name = "sample"
-  }
-}
-
 resource "aws_security_group" "allow_sample" {
-  name        = "allow_sample"
+  Name        = "allow_sample"
   description = "Allow sample traffic"
 
   ingress = [
