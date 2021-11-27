00bce2c2f43a51d63a3f84af95e336a79dedb9a3
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-27 18:52:33 +0530

    vars

--- examples/main.tf
+++ examples/main.tf
@@ -3,37 +3,35 @@ provider "aws" {
 }
 
 resource "aws_security_group" "allow_sample" {
-    name        = "allow_sample"
-    description = "Allow sample traffic"
+  name        = "allow_sample"
+  description = "Allow sample traffic"
 
-    ingress {
-      description      = "TLS from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = []
-    }
-
-    egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
-
-    tags = {
-      Name = "allow_sample"
-    }
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
   }
 
-# EC2 instance
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_sample"
+  }
+}
 
 resource "aws_instance" "sample" {
+
   ami           = "ami-0855cab4944392d0a"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security group.allow_sample.id,"sg-05e68a880b653697b"]
 
   tags = {
     Name = "sample"
