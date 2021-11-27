da59b48dbef77074032edd7897f921f8d06921e3
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-27 22:02:41 +0530

    var

--- examples/main.tf
+++ examples/main.tf
@@ -1,3 +1,44 @@
+#provider "aws" {
+#  region = "us-east-1"
+#}
+#
+#resource "aws_security_group" "sample" {
+#  name        = "allowSample"
+#  description = "Allow sample traffic"
+#
+#  ingress = [
+#    {
+#      description      = "TLS from VPC"
+#      from_port        = 22
+#      to_port          = 22
+#      protocol         = "tcp"
+#      cidr_blocks      = ["0.0.0.0/0"]
+#      ipv6_cidr_blocks = []
+#      prefix_list_ids  = []
+#      security_groups  = []
+#      self             = false
+#  }
+#]
+#  egress =[
+#   {
+#     description      ="egress"
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#     prefix_list_ids  = []
+#     security_groups  = []
+#     self             = false
+#  }
+#]
+#  tags = {
+#    Name = "allow_sample"
+#  }
+#}
+#
+#
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -17,21 +58,21 @@ resource "aws_security_group" "sample" {
       prefix_list_ids  = []
       security_groups  = []
       self             = false
-  }
-]
+    }
+  ]
   egress =[
-   {
-     description      ="egress"
-     from_port        = 0
-     to_port          = 0
-     protocol         = "-1"
-     cidr_blocks      = ["0.0.0.0/0"]
-     ipv6_cidr_blocks = ["::/0"]
-     prefix_list_ids  = []
-     security_groups  = []
-     self             = false
-  }
-]
+    {
+      description      ="egress"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
   tags = {
     Name = "sample"
   }
@@ -39,4 +80,3 @@ resource "aws_security_group" "sample" {
 output "SGID" {
   value = aws_security_group.sample.id
 }
-
