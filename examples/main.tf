b712f74dd6d06a040bf1230c27ed1d9072ab4fb3
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-28 18:03:59 +0530

    instaces

--- examples/main.tf
+++ examples/main.tf
@@ -1,43 +1,12 @@
-#provider "aws" {
-#  region = "us-east-1"
-#}
-#
-#resource "aws_security_group" "sample" {
-#  name        = "allowSample"
-#  description = "Allow sample traffic"
-#
-#  ingress = [
-#    {
-#      description      = "TLS from VPC"
-#      from_port        = 22
-#      to_port          = 22
-#      protocol         = "tcp"
-#      cidr_blocks      = ["0.0.0.0/0"]
-#      ipv6_cidr_blocks = []
-#      prefix_list_ids  = []
-#      security_groups  = []
-#      self             = false
-#  }
-#]
-#  egress =[
-#   {
-#     description      ="egress"
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#     prefix_list_ids  = []
-#     security_groups  = []
-#     self             = false
-#  }
-#]
-#  tags = {
-#    Name = "allow_sample"
-#  }
-#}
-#
-#
+# Version of terraform
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 
  provider "aws" {
   region = "us-east-1"
