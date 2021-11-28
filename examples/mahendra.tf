50031bf4a71223485bc6040028b8eb329e94fb3f
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-28 07:52:58 +0530

    instace

--- examples/mahendra.tf
+++ examples/mahendra.tf
@@ -11,3 +11,30 @@ resource "aws_instance" "mahe" {
   }
 }
 
+resource "aws_security_group" "group" {
+  name        = "group"
+  description = "Allow Group inbound traffic"
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+    ipv6_cidr_blocks = []
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "group"
+  }
+}
+
