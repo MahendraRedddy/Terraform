e9b2da4c7a15464852dce846dd82532576ea0cde
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-28 07:48:53 +0530

    instac

--- examples/mahendra.tf
+++ examples/mahendra.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region="us-east-1"
 }
 
-resource "aws_instance" "name" {
+resource "aws_instance" "mahe" {
   ami           = "ami-0855cab4944392d0a"
   instance_type = "t2.micro"
 
