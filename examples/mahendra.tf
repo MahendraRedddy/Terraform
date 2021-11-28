7ad81b9f52581e4eebe65ce72f6e0a085a3821c4
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-28 09:06:57 +0530

    instaces

--- examples/mahendra.tf
+++ examples/mahendra.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "mahe" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = " "
+    Name = "mahe"
   }
 }
 
@@ -34,14 +34,8 @@ resource "aws_security_group" "group" {
   }
 
   tags = {
-    Name = "group"
+    Name = "data"
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket = "mahendra123"
-    key    = ["examples/group/terraform.tfstate"]
-    region = "us-east-1"
-  }
-}
+
