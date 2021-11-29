40c7d391b69da8221d7e9f11bbd6b1f40503674e
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-29 18:21:13 +0530

    instacne

--- examples/main.tf
+++ examples/main.tf
@@ -1,58 +1,8 @@
-# Version of terraform
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
 
 # aws provider
  provider "aws" {
   region = "us-east-1"
  }
-
-# security group
-
-resource "aws_security_group" "sample" {
-  name        = "Sample"
-  description = "Allow sample traffic"
-
-  ingress = [
-    {
-      description      = "TLS from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
-  egress =[
-    {
-      description      ="egress"
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
-  tags = {
-    Name = "sample"
-  }
-}
-#output "SGID" {
- # value = aws_security_group.sample.id
-#}
-
 # Ec2 instance creation
 
 resource "aws_instance" "sample" {
