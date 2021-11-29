73a92e0260fb2547b862fb8220ae454f5398c809
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-29 20:54:14 +0530

    main

--- examples/main.tf
+++ examples/main.tf
@@ -1,22 +1,65 @@
-# aws provider
- provider "aws" {
-  region = "us-east-1"
- }
 
-# s3 Configuration
-
+#Version of terraform
 terraform {
-  backend "s3" {
-    bucket = "mahendra123"
-    key    = "examples/sample/terraform.tfstate"
-    region = "us-east-1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   }
 }
 
-module "ec2" {
-  source = "./ec2"
+# aws provider
+provider "aws" {
+  region = "us-east-1"
 }
 
-module "sg" {
-  source = "./sg"
+# security group
+
+resource "aws_security_group" "mahe" {
+  description = "Allow sample traffic"
+
+  ingress = [
+    {
+      description      = "TLS from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+  egress =[
+    {
+      description      ="egress"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+  tags = {
+    Name = ""
+  }
 }
+
+
+# Ec2 instance creation
+
+resource "aws_instance" "mahe" {
+  ami           = "ami-0855cab4944392d0a"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["aws_security_group.allow_sample.id","sg-05e68a880b653697b"]
+
+  tags = {
+    Name = ""
+  }
+}
+
