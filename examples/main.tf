6ba0e6fd6beb91318e57ff4cd7586b96d0153c1b
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-29 18:52:25 +0530

    instances

--- examples/main.tf
+++ examples/main.tf
@@ -5,13 +5,13 @@
  }
 # Ec2 instance creation
 
-resource "aws_instance" "sample" {
+resource "aws_instance" "mahe" {
   ami           = "ami-0855cab4944392d0a"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["aws_security_group.allow_sample.id","sg-05e68a880b653697b"]
+  vpc_security_group_ids = []
 
   tags = {
-    Name = "sample"
+    Name = "same"
   }
 }
 
@@ -20,7 +20,10 @@ resource "aws_instance" "sample" {
 terraform {
   backend "s3" {
     bucket = "mahendra123"
-    key    = ["examples/sample/terraform.tfstate"]
+    key    = ["examples/mahe/terraform.tfstate"]
     region = "us-east-1"
   }
 }
+
+
+
