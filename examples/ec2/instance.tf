b53cf3ec28b2bc67f27fef4ba8bcc42521b1b1f9
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-30 19:03:03 +0530

    instances

--- examples/ec2/instance.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "database" {
-  ami           = "ami-0855cab4944392d0a"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = []
-
-  tags = {
-    Name = "database"
-  }
-}
