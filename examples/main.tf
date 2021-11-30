0b25b198357e85cca4ea813961381cb0356fc473
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-30 19:01:49 +0530

    instance

--- examples/main.tf
+++ examples/main.tf
@@ -15,19 +15,58 @@ provider "aws" {
 }
 
 
-# s3 bucket for storage file in aws
 
-terraform {
-  backend "s3" {
-    bucket = "mahendra123"
-    key    = "examples/file/terraform.tfstate"
-    region = "us-east-1"
+resource "aws_security_group" "Security" {
+  name = "Security"
+  description = "Allow sample traffic"
+
+  ingress = [
+    {
+      description      = "TLS from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+  egress =[
+    {
+      description      ="egress"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+  tags = {
+    Name = "Group"
   }
 }
-module "ec2" {
-  source = "./ec2"
+
+resource "aws_instance" "database" {
+  ami           = "ami-0855cab4944392d0a"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.Security]
+
+  tags = {
+    Name = "Instance"
+  }
 }
 
-module "sg" {
-  source = "./sg"
-}
+# s3 bucket for storage file in aws
+
+#terraform {
+  #backend "s3" {
+  #  bucket = "mahendra123"
+   # key    = "examples/file/terraform.tfstate"
+  #  region = "us-east-1"
+ # }
+#}
