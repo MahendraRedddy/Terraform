43db96efda1c0a10d6a70879a9c61bd11d3a98f0
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-30 18:00:25 +0530

    ALL

--- examples/main.tf
+++ examples/main.tf
@@ -14,54 +14,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# security group
-
-resource "aws_security_group" "allow_mahe" {
-  description = "Allow sample traffic"
-
-  ingress = [
-    {
-      description      = "TLS from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
-  egress =[
-    {
-      description      ="egress"
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
-  tags = {
-    Name = ""
-  }
-}
-
-
-# Ec2 instance creation
-
-resource "aws_instance" "mahe" {
-  ami           = "ami-0855cab4944392d0a"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_mahe.id]
-
-  tags = {
-    Name = ""
-  }
-}
 
 # s3 bucket for storage file in aws
 
@@ -71,4 +23,11 @@ terraform {
     key    = "examples/data/terraform.tfstate"
     region = "us-east-1"
   }
+}
+module "ec2" {
+  source = "./ec2"
+}
+
+module "sg" {
+  source = "./sg"
 }
