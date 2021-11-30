734c14daabb8c42d6dec56e05025b512189b0a9d
Mahendra Reddy <eedalamahendra@gmail.com>
2021-11-30 16:00:12 +0530

    1

--- examples/main.tf
+++ examples/main.tf
@@ -56,7 +56,7 @@ resource "aws_security_group" "mahe" {
 resource "aws_instance" "mahe" {
   ami           = "ami-0855cab4944392d0a"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["aws_security_group.allow_sample.id","sg-05e68a880b653697b"]
+  vpc_security_group_ids = ["aws_security_group.allow_mahe.id","sg-05e68a880b653697b"]
 
   tags = {
     Name = ""
