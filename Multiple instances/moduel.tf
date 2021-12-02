1b39f9257ea4a7b75d3bbba2dc1aaa53bebce199
Mahendra Reddy <eedalamahendra@gmail.com>
2021-12-02 18:09:07 +0530

    multipleinstances

---
+++ Multiple instances/moduel.tf
@@ -0,0 +1,38 @@
+
+# Version of terraform
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configuration
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+# s3 bucket for storage file in aws
+
+terraform {
+  backend "s3" {
+    bucket = "mahendra123"
+    key    = "examples/database/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+module "ec2" {
+  count  = 3
+  source = "./ec2"
+  SGID =module.sg.SGID
+}
+
+
+module "sg" {
+  source = "./sg"
+}
